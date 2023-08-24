e0e548118f2a86b68a78dca74565aadeaf01a370
cbcote <christopher.cote88@gmail.com>
2023-08-23 22:19:15 -0500

    added file output methods

--- flexdb/data_exporter/core.py
+++ flexdb/data_exporter/core.py
@@ -1,59 +1,67 @@
 import json
-import datetime
-from decimal import Decimal
-from io import StringIO, BytesIO
 import pandas as pd
-import polars as pl
 import pyarrow as pa
-
+import logging
+from datetime import datetime, date
+from decimal import Decimal
 
 class DataExporter:
+    def __init__(self) -> None:
+        self.format_map = {
+            "json": self.to_json,
+            "csv": self.to_csv,
+            "excel": self.to_excel,
+        }
 
-    @staticmethod
-    def to_json(data):
+    def export_data(self, data, format_type, filename, **kwargs):
+        exporter_fn = self.format_map.get(format_type)
+        if exporter_fn:
+            exporter_fn(data, filename, **kwargs)
+        else:
+            logging.error(f"Format {format_type} not supported.")
+            return None
+
+    def to_json(self, data, filename):
         def custom_serializer(obj):
-            if isinstance(obj, (datetime.date, datetime.datetime)):
+            if isinstance(obj, (date, datetime)):
                 return obj.isoformat()
             elif isinstance(obj, Decimal):
-                return float(obj)  # or str(obj) if you prefer string representation
+                return float(obj)
             raise TypeError(f"Type {type(obj)} not serializable")
+
+        if isinstance(data, pa.lib.Table):
+            data = data.to_pandas()
 
         if isinstance(data, pd.DataFrame):
             data = data.to_dict(orient='records')
-        
+
         try:
-            return json.dumps(data, default=custom_serializer, indent=4)
+            with open(filename, 'w') as f:
+                json.dump(data, f, default=custom_serializer, indent=4)
         except Exception as e:
-            print(f"An error occurred while exporting to JSON: {e}")
-            return None
+            logging.error(f"An error occurred while exporting to JSON: {e}")
+
+    def to_csv(self, data, filename):
+        try:
+            data = self.convert_to_dataframe(data)
+            data.to_csv(filename, index=False)
+        except Exception as e:  # Adjusted from PandasError as it's not specific enough
+            logging.error(f"An error occurred while exporting to CSV: {e}")
+
+    def to_excel(self, data, filename):
+        try:
+            data = self.convert_to_dataframe(data)
+            data.to_excel(filename, index=False, engine='openpyxl')
+        except Exception as e:  # Adjusted from PandasError as it's not specific enough
+            logging.error(f"An error occurred while exporting to Excel: {e}")
 
     @staticmethod
-    def to_csv(data, column_names=None):
+    def convert_to_dataframe(data):
         if isinstance(data, list):
-            data = pd.DataFrame(data, columns=column_names)
-        elif not isinstance(data, pd.DataFrame):
-            data = pd.DataFrame(data)
-
-        output = StringIO()
-        try:
-            data.to_csv(output, index=False)
-            print(f"Data exported to {output}")
-        except Exception as e:
-            print(f"An error occurred while exporting to CSV: {e}")
-            return None
-        return output.getvalue()
-
-    @staticmethod
-    def to_excel(data, column_names=None):
-        if isinstance(data, list):
-            data = pd.DataFrame(data, columns=column_names)
-        elif not isinstance(data, pd.DataFrame):
-            data = pd.DataFrame(data)
-
-        output = BytesIO()
-        try:
-            data.to_excel(output, index=False, engine='openpyxl')
-        except Exception as e:
-            print(f"An error occurred while exporting to Excel: {e}")
-            return None
-        return output.getvalue()
+            return pd.DataFrame(data)
+        elif isinstance(data, pa.lib.Table):
+            return data.to_pandas()
+        elif isinstance(data, pd.DataFrame):
+            return data
+        else:
+            raise TypeError("Data format not recognized.")
