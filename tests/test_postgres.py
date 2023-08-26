507732eeedaed9f2eff437928d812e1db3e4ce94
cbcote <christopher.cote88@gmail.com>
2023-08-25 22:01:51 -0500

    moved test directory

---
+++ tests/test_postgres.py
@@ -0,0 +1,20 @@
+import unittest
+import yaml
+from flexdb.azure.access_token.postgresql import get_access_token
+from flexdb.connectors.postgresql import PostgreSQLConnector
+import os
+
+
+class TestPostgreSQLConnector(unittest.TestCase):
+    
+    def test_connect(self):
+        script_dir = os.path.dirname(__file__)
+
+        yaml_path = os.path.join(script_dir, '../flexdb/config/config.yaml')
+        
+        with open(yaml_path, 'r') as f:
+            config = yaml.safe_load(f)
+            config['postgresql']['password'] = get_access_token()
+        
+        connector = PostgreSQLConnector(config)
+        self.assertIsNone(connector.connect())  # Replace with an actual test
