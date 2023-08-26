124275a55ec0763b9832b99a0838f1b995993926
cbcote <christopher.cote88@gmail.com>
2023-08-25 21:20:08 -0500

    added raw sql param to read and list tables method

--- flexdb/connectors/postgresql.py
+++ flexdb/connectors/postgresql.py
@@ -37,20 +37,28 @@ class PostgreSQLConnector(DatabaseConnector):
             self.connection.rollback()
             raise
     
-    def read(self, table, filters=None, select_columns=None, output_format="dataframe"):
+    def read(self, table=None, filters=None, select_columns=None, output_format="dataframe", raw_sql=None):
         """Reads records from the specified table."""
+        
         with self.connection.cursor() as cursor:
-            if not select_columns:
-                select_string = '*'
+            
+            if raw_sql:
+                cursor.execute(raw_sql)
             else:
-                select_string = ', '.join(select_columns)
+                if not select_columns:
+                    select_string = '*'
+                else:
+                    select_string = ', '.join(select_columns)
+            
             select_query = f'SELECT {select_string} FROM {table}'
+            
             if filters:
                 conditions = ' AND '.join([f'{key} = %s' for key in filters.keys()])
                 select_query += f' WHERE {conditions}'
                 cursor.execute(select_query, list(filters.values()))
             else:
                 cursor.execute(select_query)
+            
             results = cursor.fetchall()
             column_names = [desc[0] for desc in cursor.description]
 
@@ -84,4 +92,26 @@ class PostgreSQLConnector(DatabaseConnector):
         except Exception as e:
             logging.error(f'Failed to delete data from {table}: {e}.')
             self.connection.rollback()
+            raise
+    
+    def list_tables(self):
+        """Lists all tables in the current database."""
+        try:
+            with self.connection.cursor() as cursor:
+                # This SQL query fetches all table names for the current database
+                query = """
+                SELECT tablename
+                FROM pg_catalog.pg_tables
+                WHERE schemaname != 'pg_catalog'
+                AND schemaname != 'information_schema';
+                """
+                
+                cursor.execute(query)
+                tables = cursor.fetchall()
+                # Unpack the list of tuples into a list of table names
+                table_names = [table[0] for table in tables]
+                logging.info(f"Tables in database: {table_names}")
+                return table_names
+        except Exception as e:
+            logging.error(f"Failed to list tables: {e}")
             raise
