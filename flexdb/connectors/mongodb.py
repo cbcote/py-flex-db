d3e8634f551c720afaadca004e620fd5fca4fbb4
cbcote <christopher.cote88@gmail.com>
2023-10-19 22:54:14 -0500

    mongo docstrings

--- flexdb/connectors/mongodb.py
+++ flexdb/connectors/mongodb.py
@@ -10,18 +10,51 @@ class MongoDbConnector(DatabaseConnector):
         self.connection.close()
 
     def create(self, collection, data):
+        """
+        :param collection: collection name
+        :param data: dict of data
+        :return: id of inserted document
+        
+        Example:
+        >>> db = MongoDbConnector()
+        >>> db.connect()
+        >>> db.create('users', {'name': 'John', 'age': 25})
+        ObjectId('5f1b5e5a2c9b0e5c3e4f3e5f')
+        """
         db = self.connection['mydatabase']  # replace 'mydatabase' with your db name
         coll = db[collection]
         result = coll.insert_one(data)
         return result.inserted_id
 
     def read(self, collection, filters):
+        """
+        :param collection: collection name
+        :param filters: dict of filters
+        :return: list of documents
+        
+        Example:
+        >>> db = MongoDbConnector()
+        >>> db.connect()
+        >>> db.read('users', {'name': 'John'})
+        [{'_id': ObjectId('5f1b5e5a2c9b0e5c3e4f3e5f'), 'name': 'John', 'age': 25}]
+        """
         db = self.connection['mydatabase']  # replace 'mydatabase' with your db name
         coll = db[collection]
         result = coll.find(filters)
         return list(result)
 
     def update(self, collection, filters, data):
+        """
+        :param collection: collection name
+        :param filters: dict of filters
+        :param data: dict of data
+        :return: number of modified documents
+        
+        Example:
+        >>> db = MongoDbConnector()
+        >>> db.connect()
+        >>> db.update('users', {'name': 'John'}, {'age': 26})
+        """
         db = self.connection['mydatabase']  # replace 'mydatabase' with your db name
         coll = db[collection]
         result = coll.update_one(filters, {'$set': data})
