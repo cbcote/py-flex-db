9294367eac82b82d79b8a7838179f8ba2c52f1ad
cbcote <christopher.cote88@gmail.com>
2023-10-26 23:57:17 -0500

    crud tests

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -21,3 +21,82 @@ class TestPostgreSQLConnector(unittest.TestCase):
         
         connector = PostgreSQLConnector(config)
         self.assertIsNone(connector.connect())
+    
+    def test_create(self):
+        """Create test"""
+        config = {
+            'host': os.environ.get("POSTGRES_HOST"),
+            'port': 5432,
+            'dbname': os.environ.get("POSTGRES_DB"),
+            'user': os.environ.get("POSTGRES_USER"),
+            'password': get_access_token()
+        }
+        
+        connector = PostgreSQLConnector(config)
+        connector.connect()
+        connector.create('test_table', {'column1': 'value1', 'column2': 'value2'})
+        connector.close()
+    
+    def test_read(self):
+        """Read test"""
+        config = {
+            'host': os.environ.get("POSTGRES_HOST"),
+            'port': 5432,
+            'dbname': os.environ.get("POSTGRES_DB"),
+            'user': os.environ.get("POSTGRES_USER"),
+            'password': get_access_token()
+        }
+        
+        connector = PostgreSQLConnector(config)
+        connector.connect()
+        result = connector.read('test_table', {'column1': 'value1'})
+        connector.close()
+        self.assertEqual(result, [('value1', 'value2')])
+    
+    def test_update(self):
+        """Update test"""
+        config = {
+            'host': os.environ.get("POSTGRES_HOST"),
+            'port': 5432,
+            'dbname': os.environ.get("POSTGRES_DB"),
+            'user': os.environ.get("POSTGRES_USER"),
+            'password': get_access_token()
+        }
+        
+        connector = PostgreSQLConnector(config)
+        connector.connect()
+        connector.update('test_table', {'column1': 'value1'}, {'column2': 'value3'})
+        result = connector.read('test_table', {'column1': 'value1'})
+        connector.close()
+        self.assertEqual(result, [('value1', 'value3')])
+    
+    def test_delete(self):
+        """Delete test"""
+        config = {
+            'host': os.environ.get("POSTGRES_HOST"),
+            'port': 5432,
+            'dbname': os.environ.get("POSTGRES_DB"),
+            'user': os.environ.get("POSTGRES_USER"),
+            'password': get_access_token()
+        }
+        
+        connector = PostgreSQLConnector(config)
+        connector.connect()
+        connector.delete('test_table', {'column1': 'value1'})
+        result = connector.read('test_table', {'column1': 'value1'})
+        connector.close()
+        self.assertEqual(result, [])
+    
+    def test_close(self):
+        """Close test"""
+        config = {
+            'host': os.environ.get("POSTGRES_HOST"),
+            'port': 5432,
+            'dbname': os.environ.get("POSTGRES_DB"),
+            'user': os.environ.get("POSTGRES_USER"),
+            'password': get_access_token()
+        }
+        
+        connector = PostgreSQLConnector(config)
+        connector.connect()
+        self.assertIsNone(connector.close())
