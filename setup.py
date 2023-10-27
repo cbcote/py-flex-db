7f29f2b97c15fcfd2f60eb0da44e767c955061a8
cbcote <christopher.cote88@gmail.com>
2023-10-26 23:52:55 -0500

    v1 setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Write setup file for pyflexdb package
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyflexdb',
+    version='0.1',
+    description='Python package for flexdb',
+    url='   ',
+    author='   ',
+    author_email='   ',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
