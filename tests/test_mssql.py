8f18bb2c31c87f6ea2f2b48a0662ef36c1614580
cbcote <christopher.cote88@gmail.com>
2023-10-31 22:17:41 -0500

    crud tests

--- tests/test_mssql.py
+++ tests/test_mssql.py
@@ -5,4 +5,92 @@ import os
 
 
 class TestMsSQLConnector(unittest.TestCase):
-    pass
+    
+    def test_connect(self):
+        """Connection test"""
+        config = {
+            'host': os.environ.get("MSSQL_HOST"),
+            'port': 1433,
+            'dbname': os.environ.get("MSSQL_DB"),
+            'user': os.environ.get("MSSQL_USER"),
+            'password': os.environ.get("MSSQL_PASSWORD")
+        }
+        
+        connector = MsSQLConnector(config)
+        self.assertIsNone(connector.connect())
+    
+    def test_create(self):
+        """Create test"""
+        config = {
+            'host': os.environ.get("MSSQL_HOST"),
+            'port': 1433,
+            'dbname': os.environ.get("MSSQL_DB"),
+            'user': os.environ.get("MSSQL_USER"),
+            'password': os.environ.get("MSSQL_PASSWORD")
+        }
+        
+        connector = MsSQLConnector(config)
+        connector.connect()
+        connector.create('test_table', {'column1': 'value1', 'column2': 'value2'})
+        connector.close()
+    
+    def test_read(self):
+        """Read test"""
+        config = {
+            'host': os.environ.get("MSSQL_HOST"),
+            'port': 1433,
+            'dbname': os.environ.get("MSSQL_DB"),
+            'user': os.environ.get("MSSQL_USER"),
+            'password': os.environ.get("MSSQL_PASSWORD")
+        }
+        
+        connector = MsSQLConnector(config)
+        connector.connect()
+        result = connector.read('test_table', {'column1': 'value1'})
+        connector.close()
+        self.assertEqual(result, [('value1', 'value2')])
+    
+    def test_update(self):
+        """Update test"""
+        config = {
+            'host': os.environ.get("MSSQL_HOST"),
+            'port': 1433,
+            'dbname': os.environ.get("MSSQL_DB"),
+            'user': os.environ.get("MSSQL_USER"),
+            'password': os.environ.get("MSSQL_PASSWORD")
+        }
+        
+        connector = MsSQLConnector(config)
+        connector.connect()
+        connector.update('test_table', {'column1': 'value1'}, {'column1': 'value3'})
+        connector.close()
+    
+    def test_delete(self):
+        """Delete test"""
+        config = {
+            'host': os.environ.get("MSSQL_HOST"),
+            'port': 1433,
+            'dbname': os.environ.get("MSSQL_DB"),
+            'user': os.environ.get("MSSQL_USER"),
+            'password': os.environ.get("MSSQL_PASSWORD")
+        }
+        
+        connector = MsSQLConnector(config)
+        connector.connect()
+        connector.delete('test_table', {'column1': 'value3'})
+        connector.close()
+    
+    def test_close(self):
+        """Close test"""
+        config = {
+            'host': os.environ.get("MSSQL_HOST"),
+            'port': 1433,
+            'dbname': os.environ.get("MSSQL_DB"),
+            'user': os.environ.get("MSSQL_USER"),
+            'password': os.environ.get("MSSQL_PASSWORD")
+        }
+        
+        connector = MsSQLConnector(config)
+        connector.connect()
+        connector.close()
+        self.assertIsNone(connector.connection)
